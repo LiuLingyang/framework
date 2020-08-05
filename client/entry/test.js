06823c95104e5a2dbb20af3f33be6211aa2cb8d3
tiema <tiema@pinduoduo.com>
2020-08-05 12:30:54 +0800

    update demo

--- client/entry/test.js
+++ client/entry/test.js
@@ -1,1 +1,8 @@
-console.log(111)
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>test！</div>, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
--- server/app.js
+++ server/app.js
@@ -1,25 +1,38 @@
-const koa = require('koa');
-const static = require('koa-static');
-const render = require('koa-ejs');
-const router = require('koa-router')({
-  prefix: '/'
+const koa = require("koa");
+const static = require("koa-static");
+const render = require("koa-ejs");
+const router = require("koa-router")({
+  prefix: "/",
 });
-const config = require('../config');
+const config = require("../config");
 
 const app = koa();
 app.use(router.routes());
-app.use(static(__dirname + '../'));
+app.use(static(__dirname + "../"));
 
 render(app, {
-  root: process.env.NODE_ENV === 'production' ? config.build.viewpath : config.dev.viewpath,
-  viewExt: 'ejs',
+  root:
+    process.env.NODE_ENV === "production"
+      ? config.build.viewpath
+      : config.dev.viewpath,
+  viewExt: "ejs",
   layout: false,
-  cache: false
+  cache: false,
 });
 
-router.get('/', function*() {
-  yield this.render('index', {
-    NODE_ENV: process.env.NODE_ENV || 'development'
+router.get("/", function* () {
+  yield this.render("index", {
+    NODE_ENV: process.env.NODE_ENV || "development",
+  });
+});
+router.get("/index", function* () {
+  yield this.render("index", {
+    NODE_ENV: process.env.NODE_ENV || "development",
+  });
+});
+router.get("/test", function* () {
+  yield this.render("test", {
+    NODE_ENV: process.env.NODE_ENV || "development",
   });
 });
 
